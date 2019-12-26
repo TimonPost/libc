d4491798382e211fe45ad4eb2d01fe998b89c8a4
Timon Post <timonpost@hotmail.nl>
2019-12-26 20:23:38 +0100

    added address functions

--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -27,6 +27,10 @@ pub type wchar_t = u16;
 
 pub type clock_t = i32;
 
+pub type in_port_t = u16;
+pub type sa_family_t = u8;
+pub type in_addr_t = u32;
+
 cfg_if! {
     if #[cfg(all(target_arch = "x86", target_env = "gnu"))] {
         pub type time_t = i32;
@@ -97,6 +101,40 @@ s! {
     pub struct sockaddr {
         pub sa_family: c_ushort,
         pub sa_data: [c_char; 14],
+    }
+
+    pub struct sockaddr_storage {
+        pub ss_len: u8,
+        pub ss_family: ::sa_family_t,
+        __ss_pad1: [u8; 6],
+        __ss_align: i64,
+        __ss_pad2: [u8; 112],
+    }
+
+    pub struct in_addr {
+        pub s_addr: ::in_addr_t,
+    }
+
+    #[repr(align(4))]
+    pub struct in6_addr {
+        pub s6_addr: [u8; 16],
+    }
+
+    pub struct sockaddr_in6 {
+        pub sin6_len: u8,
+        pub sin6_family: sa_family_t,
+        pub sin6_port: ::in_port_t,
+        pub sin6_flowinfo: u32,
+        pub sin6_addr: ::in6_addr,
+        pub sin6_scope_id: u32,
+    }
+
+    pub struct sockaddr_in {
+        pub sin_len: u8,
+        pub sin_family: ::sa_family_t,
+        pub sin_port: ::in_port_t,
+        pub sin_addr: ::in_addr,
+        pub sin_zero: [::c_char; 8],
     }
 }
 
